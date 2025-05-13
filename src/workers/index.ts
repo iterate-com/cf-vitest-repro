604f6334d87f430b9336421751f30de5cdce2db9
juliusmarminge <jmarminge@gmail.com>
2025-05-13 14:27:32 +0200

    openai should be imported to show error

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -1,10 +1,10 @@
-// import { OpenAI } from "openai";
+import { OpenAI } from "openai";
 
 export default {
     async fetch(request: Request) {
-        // new OpenAI({
-        //     apiKey: "sk-proj-..."
-        // })
+        new OpenAI({
+            apiKey: "sk-proj-..."
+        })
          return new Response("Hello, World!");
     }
 }
